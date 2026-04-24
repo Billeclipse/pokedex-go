a1accc0610c97a4300b35d38750d49a177a71b73
vasiliosgio <vasiliosgio@hotmail.com>
2026-04-25 02:52:51 +0300

    Add basic REPL loop

--- repl.go
+++ repl.go
@@ -1,6 +1,30 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startREPL() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(strings.TrimSpace(text)))
