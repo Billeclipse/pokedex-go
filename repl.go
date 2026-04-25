d5f7edc1365daacaa291f83ac004c305d1a1da96
vasiliosgio <vasiliosgio@hotmail.com>
2026-04-25 18:46:00 +0300

    Add PokeAPI map commands

--- repl.go
+++ repl.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	NextLocationAreaURL     string
+	PreviousLocationAreaURL string
+}
+
+type locationAreaResponse struct {
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+const locationAreaURL = "https://pokeapi.co/api/v2/location-area"
+
+var httpClient = http.Client{
+	Timeout: 10 * time.Second,
 }
 
 func getCommands() map[string]cliCommand {
@@ -25,14 +49,27 @@ func getCommands() map[string]cliCommand {
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the next 20 location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 location areas",
+			callback:    commandMapb,
+		},
 	}
 }
 
-var commandOrder = []string{"help", "exit"}
+var commandOrder = []string{"help", "exit", "map", "mapb"}
 
 func startREPL() {
 	scanner := bufio.NewScanner(os.Stdin)
 	commands := getCommands()
+	cfg := config{
+		NextLocationAreaURL: locationAreaURL,
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -52,19 +89,19 @@ func startREPL() {
 			continue
 		}
 
-		if err := command.callback(); err != nil {
+		if err := command.callback(&cfg); err != nil {
 			fmt.Println(err)
 		}
 	}
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	commands := getCommands()
 
 	fmt.Println("Welcome to the Pokedex!")
@@ -74,6 +111,50 @@ func commandHelp() error {
 	for _, name := range commandOrder {
 		command := commands[name]
 		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	return nil
+}
+
+func commandMap(cfg *config) error {
+	return fetchAndPrintLocationAreas(cfg, cfg.NextLocationAreaURL)
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.PreviousLocationAreaURL == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	return fetchAndPrintLocationAreas(cfg, cfg.PreviousLocationAreaURL)
+}
+
+func fetchAndPrintLocationAreas(cfg *config, url string) error {
+	res, err := httpClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		return fmt.Errorf("request failed with status %s", res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	areas := locationAreaResponse{}
+	if err := json.Unmarshal(body, &areas); err != nil {
+		return err
+	}
+
+	cfg.NextLocationAreaURL = areas.Next
+	cfg.PreviousLocationAreaURL = areas.Previous
+
+	for _, area := range areas.Results {
+		fmt.Println(area.Name)
 	}
 
 	return nil
